4b72671205d5e1b18013c53faf72d129a8c2ae52
dayanakaroline9 <dayanakaroline9@gmail.com>
2017-03-08 17:57:03 -0300

    blink avançado

--- dayana/dayana.ino
+++ dayana/dayana.ino
@@ -1,12 +1,14 @@
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
 }
 
-// the loop function runs over and over again forever
+// the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.println(voltage);
 }
